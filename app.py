f4acb9010f1aad6a31991315c7be835dfc9e1a58
andresok10 <gdave0930@gmail.com>
2024-10-24 15:13:42 -0500

    ok

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "hola mundo 10"
+    return "hola mundo 10 20"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
